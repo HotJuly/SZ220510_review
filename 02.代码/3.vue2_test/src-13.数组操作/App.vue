442bc3db8d2342da3c6f1c4df379fe2352e3024e
HotJuly <949588380@qq.com>
2022-11-29 09:26:39 +0800

    51.Vue1和Vue2源码之间的区别

---
+++ 02.代码/3.vue2_test/src-13.数组操作/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>arr[0]:{{arr[0]}}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      arr:[6,7,8]
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+        // this.arr[0] = 9;
+        // this.arr.splice(0,1,9);
+        // console.log(this.arr)
+
+        const arr = [6,7,8]
+        arr.splice(0,1,9);
+        console.log(arr)
+      },3000)
+  }
+}
+</script>
+
+<style>
+</style>
